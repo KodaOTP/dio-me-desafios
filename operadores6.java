fdf2df7b17a54cb2044987ed6df42c19b1c789ed
KODAOTP <thiagomvs96@hotmail.com>
2022-11-10 09:59:44 -0300

    criando primeiro commit de atividades

---
+++ operadores6.java
@@ -0,0 +1,14 @@
+public class operadores6 {
+
+    public static void main(String[] args) {
+
+    int numero1 = 1;
+    int numero2 = 2;
+
+    boolean simNao = numero1 == numero2;
+
+       System.out.println("numero um é igual a numeroDois?  " + simNao );
+        
+    }    
+    
+}
